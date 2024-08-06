20d5996076a04e8d8a4170140adc94ad6ea2ac39
Fernando Papaqui <fpapaqui@cafetosoftware.com>
2024-08-06 14:58:21 -0600

    Add methodoloy text 4 versions

--- build/blocks/edumed_rankings/index.asset.php
+++ build/blocks/edumed_rankings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '8fd2d4b436e8be1e580f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e0a6b91f4a59408287a7');
--- src/blocks/edumed_rankings/render.php
+++ src/blocks/edumed_rankings/render.php
@@ -36,29 +36,117 @@ function render_cafeto_edumed_rankings_block($attributes) {
                 <li>The existence of career placement services on campus and/or online.</li>
             </ul>
 
-            <p><em>Two: Assign weightings to eligible schools & programs</em></p>
+            <p class="rankings-popup--widget--subtitle">Two: Assign weightings to eligible schools & programs</p>
 
             <p>After creating the list of eligible schools, EduMed data scientists assigned weights and ranked schools based on a mix of metrics, which were all self-reported by the school themselves to the U.S. Department of Education and IPEDS.</p>
 
             <p>The metrics are listed below in order of most- to least-heavily weighted.</p>
 
-            <p><strong>Online Programs –&nbsp;</strong>Number of online programs, either partially- or fully-online, in the relevant subject area. Because exact numbers change often and can be difficult to verify, we use a range-based scoring system to represent this category with laptop icons.</p>
+            <ul>
+                <li><strong>Online Programs –&nbsp;</strong>Number of online programs, either partially- or fully-online, in the relevant subject area. Because exact numbers change often and can be difficult to verify, we use a range-based scoring system to represent this category with laptop icons.</li>
+                <li><strong>% in Online Ed. –&nbsp;</strong>Percent of total students taking at least one distance education class.</li>
+                <li><strong>Tuition –&nbsp;</strong>Average in-state tuition for undergraduate students studying full-time, as self-reported by the school.</li>
+                <li><strong>% Receiving Award –&nbsp;</strong>Percent of full-time, first-time students receiving an award in 6 years.</li>
+                <li><strong>Avg. Inst. Aid –&nbsp;</strong>Average amount of institutional grant aid awarded to full-time, first-time undergraduates.</li>
+                <li><strong>Student/Faculty Ratio –&nbsp;</strong>The number of students per faculty member.</li>
+                <li><strong>Student/Faculty Ratio</strong></li>
+            </ul>
 
-            <p><strong>% in Online Ed. –&nbsp;</strong>Percent of total students taking at least one distance education class.</p>
+            <p class="rankings-popup--widget--subtitle">About Our Data</p>
 
-            <p><strong>Tuition –&nbsp;</strong>Average in-state tuition for undergraduate students studying full-time, as self-reported by the school.</p>
+            <p>EduMed’s rankings use the latest official data available from <a href="https://nces.ed.gov/ipeds/" target="_blank" rel="nofollow" aria-label=" (opens in a new tab)">The Integrated Postsecondary Education Data System</a> (IPEDS). Most recent data pull: July 2024</p>',
+        '2' => '
+            <h4 class="rankings-popup--widget--title">Base Methodology for EduMed&rsquo;s &ldquo;Best Online Graduate Rankings&rdquo; for the &rsquo;24-&rsquo;25 school year.</h4>
 
-            <p><strong>% Receiving Award –&nbsp;</strong>Percent of full-time, first-time students receiving an award in 6 years.</p>
+            <p class="rankings-popup--widget--subtitle">One: Create a list of eligible schools & programs</p>
 
-            <p><strong>Avg. Inst. Aid –&nbsp;</strong>Average amount of institutional grant aid awarded to full-time, first-time undergraduates.</p>
+            <p>To be eligible for ranking, schools were required to meet the following criteria based on data pulled from The Integrated Postsecondary Education Data System (IPEDS), which was self-reported by the schools themselves.</p>
 
-            <p><strong>Student/Faculty Ratio –&nbsp;</strong>The number of students per faculty member.</p>
+            <ul>
+                <li>Institutional accreditation from an organization recognized by the U.S. Department of Education</li>
+                <li>At least one online component in a program within the ranking-subject area.</li>
+                <li>The existence of academic counseling on campus and/or online.</li>
+                <li>The existence of career placement services on campus and/or online.</li>
+            </ul>
 
-            <p><strong>Student/Faculty Ratio</strong></p>
-            <p><em>About Our Data</em> EduMed’s rankings use the latest official data available from <a href="https://nces.ed.gov/ipeds/" target="_blank" rel="nofollow" aria-label=" (opens in a new tab)">The Integrated Postsecondary Education Data System</a> (IPEDS). Most recent data pull: July 2024</p>',
-        '2' => 'This is the text for methodology option 2.',
-        '3' => 'This is the text for methodology option 3.',
-        '4' => 'This is the text for methodology option 4.',
+            <p class="rankings-popup--widget--subtitle">Two: Assign weightings to eligible schools & programs</p>
+
+            <p>After creating the list of eligible schools, EduMed data scientists assigned weights and ranked schools based on a mix of metrics, which were all self-reported by the school themselves to the U.S. Department of Education and IPEDS.</p>
+
+            <p>The metrics are listed below in order of most- to least-heavily weighted.</p>
+
+            <ul>
+                <li><strong>Online Programs –&nbsp;</strong>Number of online programs, either partially- or fully-online, in the relevant subject area. Because exact numbers change often and can be difficult to verify, we use a range-based scoring system to represent this category with laptop icons.</li>
+                <li><strong>% in Online Ed. –&nbsp;</strong>Percent of graduate students taking at least one distance education class.</li>
+                <li><strong>Tuition –&nbsp;</strong>Average in-state tuition for graduate students studying full-time, as self-reported by the school.</li>
+                <li><strong>Student/Faculty Ratio –&nbsp;</strong>The number of students per faculty member.</li>
+            </ul>
+
+            <p class="rankings-popup--widget--subtitle">About Our Data</p>
+            
+            <p>EduMed’s rankings use the latest official data available from <a href="https://nces.ed.gov/ipeds/" target="_blank" rel="nofollow" aria-label=" (opens in a new tab)">The Integrated Postsecondary Education Data System</a> (IPEDS). Most recent data pull: July 2024</p>',
+        '3' => '
+            <h4 class="rankings-popup--widget--title">Base Methodology for EduMed&rsquo;s &ldquo;Most Affordable Online College Rankings&rdquo; for the &lsquo;24-&rsquo;25 school year.</h4>
+
+            <p class="rankings-popup--widget--subtitle">One: Create a list of eligible schools & programs</p>
+
+            <p>To be eligible for ranking, schools were required to meet the following criteria based on data pulled from The Integrated Postsecondary Education Data System (IPEDS), which was self-reported by the schools themselves.</p>
+
+            <ul>
+                <li>Institutional accreditation from an organization recognized by the U.S. Department of Education</li>
+                <li>At least one online component in a program within the ranking-subject area.</li>
+                <li>The existence of academic counseling on campus and/or online.</li>
+                <li>The existence of career placement services on campus and/or online.</li>
+            </ul>
+
+            <p class="rankings-popup--widget--subtitle">Two: Assign weightings to eligible schools & programs</p>
+
+            <p>After creating the list of eligible schools, EduMed data scientists assigned weights and ranked schools based on a mix of metrics, which were all self-reported by the school themselves to the U.S. Department of Education and IPEDS.</p>
+
+            <p>The metrics are listed below in order of most- to least-heavily weighted.</p>
+
+            <ul>
+                <li><strong>Tuition –&nbsp;</strong>Average in-state tuition for undergraduate students studying full-time, as self-reported by the school.</li>
+                <li><strong>Avg. Inst. Aid –&nbsp;</strong>Average amount of institutional grant aid awarded to full-time, first-time undergraduates.</li>
+                <li><strong>Online Programs –&nbsp;</strong>Number of online programs, either partially- or fully-online, in the relevant subject area. Because exact numbers change often and can be difficult to verify, we use a range-based scoring system to represent this category with laptop icons.</li>
+                <li><strong>% Receiving Award –&nbsp;</strong>Percent of full-time, first-time students receiving an award in 6 years.</li>
+                <li><strong>% in Online Ed. –&nbsp;</strong>Percent of total students taking at least one distance education class.</li>
+                <li><strong>Student/Faculty Ratio –&nbsp;</strong>The number of students per faculty member.</li>
+            </ul>
+
+            <p class="rankings-popup--widget--subtitle">About Our Data</p>
+            
+            <p>EduMed’s rankings use the latest official data available from <a href="https://nces.ed.gov/ipeds/" target="_blank" rel="nofollow" aria-label=" (opens in a new tab)">The Integrated Postsecondary Education Data System</a> (IPEDS). Most recent data pull: July 2024</p>',
+        '4' => '
+            <h4 class="rankings-popup--widget--title">Base Methodology for EduMed&rsquo;s &ldquo;Most Affordable Online Graduate Rankings&rdquo; for the &rsquo;24-&rsquo;25 school year.</h4>
+
+            <p class="rankings-popup--widget--subtitle">One: Create a list of eligible schools & programs</p>
+
+            <p>To be eligible for ranking, schools were required to meet the following criteria based on data pulled from The Integrated Postsecondary Education Data System (IPEDS), which was self-reported by the schools themselves.</p>
+
+            <ul>
+                <li>Institutional accreditation from an organization recognized by the U.S. Department of Education</li>
+                <li>At least one online component in a program within the ranking-subject area.</li>
+                <li>The existence of academic counseling on campus and/or online.</li>
+                <li>The existence of career placement services on campus and/or online.</li>
+            </ul>
+
+            <p class="rankings-popup--widget--subtitle">Two: Assign weightings to eligible schools & programs</p>
+
+            <p>After creating the list of eligible schools, EduMed data scientists assigned weights and ranked schools based on a mix of metrics, which were all self-reported by the school themselves to the U.S. Department of Education and IPEDS.</p>
+
+            <p>The metrics are listed below in order of most- to least-heavily weighted.</p>
+
+            <ul>
+                <li><strong>Tuition –&nbsp;</strong>Average in-state tuition for graduate students studying full-time, as self-reported by the school.</li>
+                <li><strong>Online Programs –&nbsp;</strong>Number of online programs, either partially- or fully-online, in the relevant subject area. Because exact numbers change often and can be difficult to verify, we use a range-based scoring system to represent this category with laptop icons.</li>
+                <li><strong>% in Online Ed. –&nbsp;</strong>Percent of graduate students taking at least one distance education class.</li>
+                <li><strong>Student/Faculty Ratio –&nbsp;</strong>The number of students per faculty member.</li>
+            </ul>
+
+            <p class="rankings-popup--widget--subtitle">About Our Data</p>
+            
+            <p>EduMed’s rankings use the latest official data available from <a href="https://nces.ed.gov/ipeds/" target="_blank" rel="nofollow" aria-label=" (opens in a new tab)">The Integrated Postsecondary Education Data System</a> (IPEDS). Most recent data pull: July 2024</p>',
     );
 
     // Set agruments for query
@@ -279,7 +367,7 @@ function render_cafeto_edumed_rankings_block($attributes) {
 
         <section class="rankings-popup">
             <div class="rankings-popup--widget rankings-popup--2024 hidden">
-                <span class="rankings-popup--widget--close">x</span>
+                <span class="rankings-popup--widget--close">X</span>
                 <?php 
                 if (!empty($posts)) {
                     foreach ($posts as $post) {

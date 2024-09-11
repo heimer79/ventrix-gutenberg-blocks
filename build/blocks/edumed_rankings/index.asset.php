2ed176189382d96e9603b6820592b2f49baca93e
Fernando Papaqui <fpapaqui@cafetosoftware.com>
2024-09-11 08:04:42 -0600

    Remove unused REST API code and fix - + icon position on mobile for EduMed rankings

--- build/blocks/edumed_rankings/index.asset.php
+++ build/blocks/edumed_rankings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'e9bf00fdf8120c920f15');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '7cc2f1a942482a1d2db5');

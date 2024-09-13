a52569d8e90b9ebb386443dbee9afbf65d4189b9
Fernando Papaqui <fpapaqui@cafetosoftware.com>
2024-09-13 13:18:42 -0600

    Compile

--- build/blocks/psd_rankings/index.asset.php
+++ build/blocks/psd_rankings/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '6395eb673088ddb13584');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'c6a0e3618e6ba33e1ab7');

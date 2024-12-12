e18002baca3494154e3b921db41acbe075522f25
jcvtx <jcortes.ventrix@gmail.com>
2024-12-12 12:23:09 -0500

    feature/multipurpose-card-block

---
+++ src/blocks/multipurpose-card/index.js
@@ -0,0 +1,62 @@
+/**
+ * Registers a new block provided a unique name and an object defining its behavior.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+import { registerBlockType } from '@wordpress/blocks';
+
+// Import edit and save components
+import Edit from './edit';
+import Save from './save';
+import metadata from './block.json';
+
+/**
+ * Every block starts by registering a new block type definition.
+ *
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-registration/
+ */
+registerBlockType( metadata.name, {
+    
+    attributes: {
+        borderColor: {
+            type: 'string',
+            default: '#E3DBFF', // Default is #E3DBFF color
+        },
+        backgroundColor: {
+            type: 'string',
+            default: '#F8F7FF', // Default is #F8F7FF color
+        },
+        paddingInline: {
+            type: 'string',
+            default: '50px', // Default inline padding
+        },
+        paddingBlock: {
+            type: 'string',
+            default: '40px', // Default block padding
+        },
+        borderRadiusTopLeft: {
+            type: 'string',
+            default: '30px', // Default border-radius for top-left
+        },
+        borderRadiusTopRight: {
+            type: 'string',
+            default: '30px', // Default border-radius for top-right
+        },
+        borderRadiusBottomLeft: {
+            type: 'string',
+            default: '30px', // Default border-radius for bottom-left
+        },
+        borderRadiusBottomRight: {
+            type: 'string',
+            default: '30px', // Default border-radius for bottom-right
+        },
+    },
+    supports: {
+        className: true, // Enables the "Additional CSS Classes" field
+    },
+	/**
+	 * @see ./edit.js
+	 */
+	edit: Edit,
+	save: Save,
+} );
---
+++ src/blocks/multipurpose-card/save.js
@@ -0,0 +1,47 @@
+import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
+import './style.scss';
+
+/**
+ * The save function handles how the block's content is saved to the database.
+ * 
+ * @param {Object} props - The block properties.
+ * @param {Object} props.attributes - Attributes of the block.
+ * 
+ * @returns {JSX.Element} - The block's saved HTML structure.
+ */
+const Save = ({ attributes }) => {
+    const {
+        borderColor,
+        backgroundColor,
+        paddingInline,
+        paddingBlock,
+        borderRadiusTopLeft,
+        borderRadiusTopRight,
+        borderRadiusBottomLeft,
+        borderRadiusBottomRight,
+    } = attributes;
+
+    // Generate block props with dynamic background color
+    const blockProps = useBlockProps.save({
+        className: 'ventrix-multipurpose-card-block', // Permanent class
+        style: {
+            borderColor: borderColor || undefined,
+            backgroundColor: backgroundColor || undefined,
+            paddingInline: paddingInline || undefined,
+            paddingBlock: paddingBlock || undefined,
+            borderTopLeftRadius: borderRadiusTopLeft || undefined,
+            borderTopRightRadius: borderRadiusTopRight || undefined,
+            borderBottomLeftRadius: borderRadiusBottomLeft || undefined,
+            borderBottomRightRadius: borderRadiusBottomRight || undefined,
+        },
+        
+    });
+
+    return (
+        <div {...blockProps}>
+            <InnerBlocks.Content />
+        </div>
+    );
+};
+
+export default Save;

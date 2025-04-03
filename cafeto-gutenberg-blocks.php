66e222a36b0ca2a1d9abd7ddc3e0c0628915f3be
heimer79 <heimer79@gmail.com>
2025-04-03 16:17:02 -0500

    feat: add OMD styles for salaries and careers tables in mobile and desktop views

--- cafeto-gutenberg-blocks.php
+++ cafeto-gutenberg-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Custom Gutenberg blocks created by the Ventrix Dev Team.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           2.0.2
+ * Version:           2.1.0
  * Author:            Ventrix Dev Team
  * Author URI:        https://ventrixadvertising.com/
  * License:           GPL-2.0-or-later

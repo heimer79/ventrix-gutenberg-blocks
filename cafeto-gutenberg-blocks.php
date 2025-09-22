c8ee2aaa4c8fe2d07a1f9544d77ab5cbb08a1a22
Miguel Buriticá Osorno <buritica1195@gmail.com>
2025-09-22 16:50:30 -0500

    Add plugin docs and improve rankings block layout
    Added README.md, CHANGELOG.md, and readme.txt for plugin documentation. Updated main plugin file and created plugin subfolder for deployment. Improved ACF field labels for rankings 2026, and updated the edumed_rankings block to conditionally display the 'Students w/ Aid' column and adjust grid layouts accordingly in both PHP and SCSS.

--- cafeto-gutenberg-blocks.php
+++ cafeto-gutenberg-blocks.php
@@ -5,7 +5,7 @@
  * Description:       Custom Gutenberg blocks created by the Ventrix Dev Team.
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           3.4.3
+ * Version:           3.4.4
  * Author:            Ventrix Dev Team
  * Author URI:        https://ventrixadvertising.com/
  * License:           GPL-2.0-or-later
